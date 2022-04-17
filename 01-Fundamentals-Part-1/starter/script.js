39f3229dfdff2b34eaa11de7d3da9b7a9ad36fe4
smcdev <smcdev@outlook.com>
2022-04-17 02:18:07 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -14,11 +14,28 @@ console.log(firstName);
 let jonas_matilda = 'JM';
 let $function = 27;
 
-let person = 'jonas'
+let person = 'jonas';
 let PI = 3.14415; //constant is all caps
 
-let myFirstJob = 'Programmer'
-let myCurrentJob = 'Teacher'
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
 
-let job1 = 'Programmer'
-let job2 = 'Teacher'
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
+
+// LECTURE: Values and Variables 
+// 1. Declare variables called 'country', 'continent' and 'population' and 
+// assign their values according to your own country (population in millions) 
+// 2. Log their values to the console
+
+let country = 'United States of America';
+let continent = 'North America';
+let population = 329;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Data Types
