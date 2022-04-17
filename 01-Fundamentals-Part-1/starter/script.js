82d8c80446e97ff90141466001e71cf1092cb1f0
smcdev <smcdev@outlook.com>
2022-04-17 14:39:45 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -91,7 +91,7 @@ const birthYear = 1991;
 var job = 'programmer';
 job = 'teacher';
 
-lastName = 'Schmedtmann'
+lastName = 'Schmedtmann';
 console.log(lastName);
 
 // LECTURE: let, const and var 
@@ -102,7 +102,113 @@ console.log(lastName);
 // 3. Try to change one of the changed variables now, and observe what happens
 
 language = 'English';
-country = Russia;
+//country = Russia;
 
 ////////////////////////////////////
 //Basic operators
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstNames = 'Jonas';
+const lastNames = 'Schmedtmann';
+console.log(firstNames + ' ' + lastNames);
+
+let x = 10 + 5; //15
+x += 10; //25
+x *= 4; //100
+x ++; //101
+x --; // 99
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now -2018);
+
+let xx, y;
+xx = y = 25 - 10 - 5;
+console.log(xx, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+
+// LECTURE: Basic Operators 
+// 1. If your country split in half, and each half would contain half the population, 
+// then how many people would live in each half? 
+// 2. Increase the population of your country by 1 and log the result to the console 
+// 3. Finland has a population of 6 million. Does your country have more people than 
+// Finland? 
+// 4. The average population of a country is 33 million people. Does your country 
+// have less people than the average country? 
+// 5. Based on the variables you created, create a new variable 'description' 
+// which contains a string with this format: 'Portugal is in Europe, and its 11 million 
+// people speak portuguese'
+
+let countrySplit = population / 2;
+population ++
+console.log(population);
+
+let finlandPopulation = 6;
+population > finlandPopulation;
+
+let averagePopulation = 33;
+population < averagePopulation;
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese'
+
+////////////////////////////////////
+// Coding Challenge #1 
+// Mark and John are trying to compare their BMI (Body Mass Index), which is 
+// calculated using the formula: 
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+// and height in meter). 
+// Your tasks: 
+// 1. Store Mark's and John's mass and height in variables 
+// 2. Calculate both their BMIs using the formula (you can even implement both 
+// versions) 
+// 3. Create a Boolean variable 'markHigherBMI' containing information about 
+// whether Mark has a higher BMI than John. 
+// Test data: 
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
+// m tall. 
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 
+// m tall. 
+ 
+// GOOD LUCK 😀
+
+//Test Data 1
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let bmiMark = markWeight / markHeight ** 2 / (markHeight * markHeight);
+let bmiJohn = johnWeight / johnHeight ** 2 / (johnHeight * johnHeight);
+console.log(bmiMark);
+console.log(bmiJohn);
+
+let markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI);
+
+//Test Data 2
+markWeight = 95;
+markHeight = 1.88;
+johnWeight = 85;
+johnHeight = 1.76;
+
+bmiMark = markWeight / markHeight ** 2 / (markHeight * markHeight)
+bmiJohn = johnWeight / johnHeight ** 2 / (johnHeight * johnHeight)
+console.log(bmiMark);
+console.log(bmiJohn);
+
+markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI);
+
+////////////////////////////////////
