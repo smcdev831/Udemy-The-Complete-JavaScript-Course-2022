ebf13409def28d50d93659b856425a1cd3e7e4ee
smcdev <smcdev@outlook.com>
2022-04-17 01:32:22 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,12 @@
+//linking Javascript file
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+//Values and variables
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda'
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
