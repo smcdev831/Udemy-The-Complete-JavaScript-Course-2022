ad625a7957b5049c6cb0b6813edf80a3622d9e6b
smcdev <smcdev@outlook.com>
2022-04-17 21:05:53 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -343,3 +343,38 @@ if (bmiMark > bmiJohn) {
 
 ////////////////////////////////////
 //Type Conversion and Coercion
+
+let inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+console.log('I am ' + 23 + 'years old.');
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+
+let n = '1' + 1;
+n = n - 1
+console.log(n);
+
+2+3+4+'5' //95
+'10' - '4' - '3' - 2 + '5' //15
+
+// LECTURE: Type Conversion and Coercion 
+// 1. Predict the result of these 5 operations without executing them: 
+// '9' - '5'; 
+// '19' - '13' + '17'; 
+// '19' - '13' + 17; 
+// '123' < 57; 
+// 5 + 6 + '4' + 9 - 4 - 2; 
+// 2. Execute the operations to check if you were right
+
+console.log('9' - '5'); //4
+console.log('19' - '13' + '17'); //617
+console.log('19' - '13' + 17); //23
+console.log('123' < 57); //True
+console.log(5 + 6 + '4' + 9 - 4 - 2); //1143
