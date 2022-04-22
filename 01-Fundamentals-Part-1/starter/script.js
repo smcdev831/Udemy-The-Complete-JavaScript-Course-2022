9ce81eeed7abba295e7a245559e8c13374118dc1
smcdev <smcdev@outlook.com>
2022-04-21 23:40:28 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -518,7 +518,6 @@ if (population < 50 && !isIsland && language === 'English') {
 
 ////////////////////////////////////
 // Coding Challenge #3
-// Coding Challenge #3 
 // There are two gymnastics teams, Dolphins and Koalas. They compete against each 
 // other 3 times. The winner with the highest average score wins a trophy! 
 // Your tasks: 
@@ -686,8 +685,26 @@ switch (language) {
 ////////////////////////////////////
 //Statements and Expressions
 
+
 ////////////////////////////////////
 //The Conditional (Ternary)
+
+let ageT = 23;
+
+ageT >= 18 ? console.log('I like to drink wine.') : console.log('I like to drink water.');
+
+let drink = ageT >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (ageT >= 18) {
+    drink2 = 'wine'
+} else {
+    drink2 = 'water'
+}
+console.log(drink2);
+
+console.log(`I like to drink ${ageT >= 18 ? 'wine' : 'water'}`);
 
 // LECTURE: The Conditional (Ternary) Operator 
 // 1. If your country's population is greater than 33 million, use the ternary operator 
@@ -695,4 +712,26 @@ switch (language) {
 // Otherwise, simply log 'Portugal's population is below average'. Notice how only 
 // one word changes between these two sentences! 
 // 2. After checking the result, change the population temporarily to 13 and then to 
-// 130. See the different results, and set the population back to original 
+// 130. See the different results, and set the population back to original
+
+population >= 33 ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`);
+
+////////////////////////////////////
+// Coding Challenge #4 
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a 
+// restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 
+// 300. If the value is different, the tip is 20%. 
+// Your tasks: 
+// 1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for 
+// this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can 
+// start with an if/else statement, and then try to convert it to a ternary 
+// operator!) 
+// 2. Print a string to the console containing the bill value, the tip, and the final value 
+// (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 
+// 316.25” 
+// Test data: 
+// § Data 1: Test for bill values 275, 40 and 430 
+// Hints: 
+// § To calculate 20% of a value, simply multiply it by 20/100 = 0.2 
+// § Value X is between 50 and 300, if it's >= 50 && <= 300 😉 
+// GOOD LUCK 😀
