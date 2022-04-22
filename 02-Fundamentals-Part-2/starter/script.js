568cf8009e05bf90982daf647d66a7250705f44e
smcdev <smcdev@outlook.com>
2022-04-22 00:20:48 -0500

    Update script.js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,23 @@
+////////////////////////////////////
+// Functions
+
+'use strict';
+
+let hasDriversLicense = false;
+let passTest = true;
+
+if (passTest) {
+    hasDriversLicense = true;
+}
+
+if (hasDriversLicense) {
+    console.log('I can drive :D');
+}
+
+// let interface = 'Audio';
+// let private = 534;
+
+////////////////////////////////////
+// Functions
+
+
