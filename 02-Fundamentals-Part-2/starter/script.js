dbb1944fe1da1b2e32c6c8aeb0ef01d857ae4c5c
smcdev <smcdev@outlook.com>
2022-04-26 14:46:42 -0500

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -250,3 +250,56 @@ checkWinner(44, 23, 71, 65, 54, 49)
 
 //Test Data 2
 checkWinner(85, 54, 41, 23, 34, 27)
+
+////////////////////////////////////
+//Intro to Arrays
+
+// LECTURE: Introduction to Arrays 
+// 1. Create an array containing 4 population values of 4 countries of your choice. 
+// You may use the values you have been using previously. Store this array into a 
+// variable called 'populations' 
+// 2. Log to the console whether the array has 4 elements or not (true or false) 
+// 3. Create an array called 'percentages' containing the percentages of the 
+// world population for these 4 population values. Use the function 
+// 'percentageOfWorld1' that you created earlier to compute the 4 
+// percentage values
+
+////////////////////////////////////
+// Basic Array Operations
+
+// LECTURE: Basic Array Operations (Methods) 
+// 1. Create an array containing all the neighbouring countries of a country of your 
+// choice. Choose a country which has at least 2 or 3 neighbours. Store the array 
+// into a variable called 'neighbours' 
+// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of 
+// your selected country. So add it to the end of the 'neighbours' array 
+// 3. Unfortunately, after some time, the new country is dissolved. So remove it from 
+// the end of the array 
+// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the 
+// console: 'Probably not a central European country :D' 
+// 5. Change the name of one of your neighbouring countries. To do that, find the 
+// index of the country in the 'neighbours' array, and then use that index to 
+// change the array at that index position. For example, you can search for 
+// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+////////////////////////////////////
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of 
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is 
+// 20%. 
+// Your tasks: 
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns 
+// the corresponding tip, calculated based on the rules above (you can check out 
+// the code from first tip calculator challenge if you need to). Use the function 
+// type you like the most. Test the function using a bill value of 100 
+// 2. And now let's use arrays! So create an array 'bills' containing the test data 
+// below 
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from 
+// the function you created before 
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip 
+// Test data: 125, 555 and 44 
+// Hint: Remember that an array needs a value in each position, and that value can 
+// actually be the returned value of a function! So you can just call a function as array 
+// values (so don't store the tip values in separate variables first, but right in the new 
+// array) 😉 
+// GOOD LUCK 😀
