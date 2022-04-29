6b47c4e68c2fb35bfe615f147ab1ec656aa3bbb8
smcdev <smcdev@outlook.com>
2022-04-29 00:37:54 -0500

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -523,3 +523,48 @@ if (mark.calcBMI() > john.calcBMI()) {
 } else {
     console.log(`John's BMI (${john.calcBMI()}) is higher than Mark's (${mark.calcBMI()})!`);
 }
+
+////////////////////////////////////
+//Iteration: The for loop
+
+// LECTURE: Iteration: The for Loop 
+// 1. There are elections in your country! In a small town, there are only 50 voters. Use a for loop to simulate the 50 people voting, by logging a string like this to the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
+
+////////////////////////////////////
+//Looping arrays, Breaking and Continuing
+
+// LECTURE: Looping Arrays, Breaking and Continuing 
+// 1. Let's bring back the 'populations' array from a previous assignment 
+// 2. Use a for loop to compute an array called 'percentages2' containing the percentages of the world population for the 4 population values. Use the function 'percentageOfWorld1' that you created earlier 
+// 3. Confirm that 'percentages2' contains exactly the same values as the 'percentages' array that we created manually in the previous assignment, and reflect on how much better this solution is
+
+////////////////////////////////////
+//Looping backwards and Loops in loops
+
+// LECTURE: Looping Backwards and Loops in Loops 
+// 1. Store this array of arrays into a variable called 'listOfNeighbours' [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']]; 
+// 2. Log only the neighbouring countries to the console, one by one, not the entire arrays. Log a string like 'Neighbour: Canada' for each country 
+// 3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't worry if it's too difficult for you! But you can still try to figure this out anyway 😉
+
+////////////////////////////////////
+//The while loop
+
+// LECTURE: The while Loop 
+// 1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing', but this time using a while loop (call the array 'percentages3') 
+// 2. Reflect on what solution you like better for this task: the for loop or the while loop?
+
+////////////////////////////////////
+// Coding Challenge #4
+// Let's improve Steven's tip calculator even more, this time using loops! 
+// Your tasks: 
+// 1. Create an array 'bills' containing all 10 test bill values 
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals') 
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations! 
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52 
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the tips and totals arrays 😉 
+// Bonus: 
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it: 
+// 4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together 
+// 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements) 
+// 4.3. Call the function with the 'totals' array 
+// GOOD LUCK 😀
