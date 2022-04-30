ec418addf82720ae5afdf0ee595577a4fdf065f0
smcdev <smcdev@outlook.com>
2022-04-30 17:33:02 -0500

    Update script.js

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
+////////////////////////////////////
+//Setting up Prettier and VS Code
+
 let x = 23;
 if (x === 23) console.log(23);
 
@@ -8,6 +11,41 @@ let calcAge = (birthYear) => 2037 - birthYear;
 
 console.log(calcAge(1991));
 console.log(calcAge(1950));
+
+////////////////////////////////////
+//Using Google, Stackoverflow, and MDN
+
+//We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+let temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+function calcTempAmplitude(arr) {
+  let maxTemp = 0;
+  let minTemp = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number") {
+      continue;
+    } else if (arr[i] > maxTemp) {
+      maxTemp = arr[i];
+    } else if (arr[i] < minTemp) {
+      minTemp = arr[i];
+    }
+  }
+  console.log(minTemp, maxTemp, minTemp - maxTemp);
+}
+
+calcTempAmplitude(temperatures);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+let temps1 = [3, 5, 1];
+let temps2 = [9, 0, 5];
+
+let temperatures1 = temps1.concat(temps2);
+console.log(temperatures1);
+
+calcTempAmplitude(temperatures1);
 
 ////////////////////////////////////
 // Coding Challenge #1
