1a9aa85f58604514afbc9adb4c2562ff9c81cf28
smcdev <smcdev@outlook.com>
2022-04-30 01:15:36 -0500

    Update script.js

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -19,3 +19,15 @@ console.log(calcAge(1950));
 // § Data 1: [17, 21, 23]
 // § Data 2: [12, 5, -5, 0, 4]
 // GOOD LUCK 😀
+
+let data1 = [17, 21, 23];
+let data2 = [12, 5, -5, 0, 4];
+
+function printForecase(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}ºC in ${i + 1} days`);
+  }
+}
+
+printForecase(data1);
+printForecase(data2);
