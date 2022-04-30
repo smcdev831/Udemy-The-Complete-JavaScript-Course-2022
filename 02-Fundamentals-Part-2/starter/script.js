c54cd8e5ad61a21b9d5785592f9a48780d5ac288
smcdev <smcdev@outlook.com>
2022-04-29 19:10:20 -0500

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -527,8 +527,27 @@ if (mark.calcBMI() > john.calcBMI()) {
 ////////////////////////////////////
 //Iteration: The for loop
 
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 2');
+console.log('Lifting weights repetition 3');
+console.log('Lifting weights repetition 4');
+console.log('Lifting weights repetition 5');
+console.log('Lifting weights repetition 6');
+console.log('Lifting weights repetition 7');
+console.log('Lifting weights repetition 8');
+console.log('Lifting weights repetition 9');
+console.log('Lifting weights repetition 10');
+
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
 // LECTURE: Iteration: The for Loop 
 // 1. There are elections in your country! In a small town, there are only 50 voters. Use a for loop to simulate the 50 people voting, by logging a string like this to the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
+
+for (let voter = 1; voter <=50; voter++) {
+    console.log(`Voter number ${voter} is currently voting`);
+}
 
 ////////////////////////////////////
 //Looping arrays, Breaking and Continuing
