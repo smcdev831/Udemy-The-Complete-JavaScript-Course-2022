fc0d233d0e2a1c58b214afacfde920f66fd30562
smcdev <smcdev@outlook.com>
2022-05-01 18:23:04 -0500

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".message").textContent = "Correct Number!";
+
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
 
 // Coding Challenge #1
 // Implement a game rest functionality, so that the player can make a new guess!
