ed538419170fbd9db3d4a9a0df8142010f87cbe3
smcdev <smcdev@outlook.com>
2022-05-01 21:04:16 -0500

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,18 +12,34 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
-let x = function () {
-  console.log(23);
-};
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
   let guess = Number(document.querySelector(".guess").value);
   console.log(guess);
-  console.log(typeof guess);
+  console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector(".message").textContent = "No number!";
+    document.querySelector(".message").textContent = "🚨 No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number! 🎉";
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      score = document.querySelector(".score").textContent = score - 1;
+      document.querySelector(".message").textContent = "🔼 Guess is too high!";
+    } else {
+      document.querySelector(".message").textContent = "You lose!";
+    }
   } else {
+    if (score > 0) {
+      score--;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent = "🔽 Guess is too low!";
+    } else {
+      document.querySelector(".message").textContent = "You lose!";
+    }
   }
 });
 
