49c724db854f2d2e7f88bd42ac5203ee8f966938
smcdev <smcdev@outlook.com>
2022-05-02 20:36:51 -0500

    Update script.js

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
-'use strict';
+"use strict";
+
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let close = document.querySelector(".close-modal");
+let show = document.querySelectorAll(".show-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < show.length; i++) {
+  show[i].addEventListener("click", openModal);
+}
+
+close.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
