e5d34e255325cffbf2ebe57d62ed73d747481236
smcdev <smcdev@outlook.com>
2022-05-03 21:42:52 -0500

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+let score0 = document.querySelector("#score--0");
+let score1 = document.querySelector("#score--1");
+let diceEl = document.querySelector(".dice");
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add("hidden");
