86f79b160e9328e6880112c3431d9b9d7dfe0d13
smcdev <smcdev@outlook.com>
2022-05-04 00:11:54 -0500

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,13 +2,18 @@
 
 let score00 = document.querySelector("#score--0");
 let current00 = document.getElementById("current--0");
+let player00 = document.querySelector(".player--0");
 let score11 = document.querySelector("#score--1");
 let current11 = document.getElementById("current--1");
+let player01 = document.querySelector(".player--1");
 let diceEl = document.querySelector(".dice");
 let btnNew = document.querySelector(".btn--new");
 let btnRoll = document.querySelector(".btn--roll");
 let btnHold = document.querySelector(".btn--hold");
+let currentPlayer = 0;
+let activePlayer = 0;
 let currentScore = 0;
+let scores = [0, 0];
 
 score00.textContent = 0;
 score11.textContent = 0;
@@ -20,9 +25,16 @@ btnRoll.addEventListener("click", function () {
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
 
-  if (dice != 1) {
+  if (dice !== 1) {
     currentScore += dice;
-    current00.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player00.classList.toggle("player--active");
+    player01.classList.toggle("player--active");
   }
 });
