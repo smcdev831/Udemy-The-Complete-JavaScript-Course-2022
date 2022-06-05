6d27f847e31b031896d58c0345e7ccfb4219ddc5
smcdev <smcdev@outlook.com>
2022-06-05 01:41:45 -0500

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -344,20 +344,36 @@ const game = {
   date: "Nov 9th, 2037",
   odds: {
     team1: 1.33,
-    draw: 3.25,
+    x: 3.25,
     team2: 6.5,
   },
-  printGoals: function (...playerNames) {
-    console.log(...playerNames, playerNames.length);
+  printGoals: function (...players) {
+    console.log(`${players.length} goals were scored by`, ...players);
   },
 };
 
-let players1 = game.players[0];
-let players2 = game.players[1];
-let gk = players1[0];
-let fieldPlayers = [...players1];
+// let players1 = game.players[0];
+// let players2 = game.players[1];
+// let gk = players1[0];
+// let fieldPlayers = [...players1];
+// let allPlayers = [...players1, ...players2];
+// let players1Final = [...fieldPlayers, "Thiago", "Coutinho", "Perisic"];
+
+//Class answers
+let [players1, players2] = game.players;
+let [gk, ...fieldPlayers] = players1;
 let allPlayers = [...players1, ...players2];
-let players1Final = [...fieldPlayers, "Thiago", "Coutinho", "Perisic"];
+let players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+
+let {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+game.printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+game.printGoals(...game.scored);
+
+team1 < team2 && console.log("Team 1 is more likely to win");
+team1 > team2 && console.log("Team 2 is more likely to win");
 
 ////////////////////////////////////
 // Coding Challenge #2
