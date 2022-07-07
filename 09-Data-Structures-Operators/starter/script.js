27050b5e2e8470964bea363f146cc11282125e3a
smcdev <smcdev@outlook.com>
2022-07-06 21:49:36 -0500

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -591,6 +591,8 @@ console.log(
   new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
 );
 
+console.log(new Set("jonasschmedtmann").size);
+
 ////////////////////////////////////
 // Coding Challenge #3
 // Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
