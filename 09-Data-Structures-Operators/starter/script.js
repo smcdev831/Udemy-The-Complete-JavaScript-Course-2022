5ab2f2b86bcfa6f6b6964368419461dac55895b0
smcdev <smcdev@outlook.com>
2022-07-06 22:39:36 -0500

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -599,7 +599,25 @@ console.log(new Set("jonasschmedtmann").size);
 let rest = new Map();
 rest.set("name", "Classico Italiano");
 rest.set(1, "Firenze, Italy");
-rest.set(2, "Lisbon, Portugal");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open!")
+  .set(false, "We are closed!");
+
+console.log(rest);
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+let time = 21;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
 console.log(rest);
 
 ////////////////////////////////////
