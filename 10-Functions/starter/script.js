4f5cb2713e7fa1d1e5088fbf3a82c7d37e706efd
smcdev <smcdev@outlook.com>
2022-09-18 23:00:23 -0500

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -3,6 +3,28 @@
 ////////////////////////////////////
 // Default Parameters
 
+let bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
+createBooking("LH123", undefined, 1000);
+
 ////////////////////////////////////
 // How Passing Arguments Works: Value vs Reference
 
