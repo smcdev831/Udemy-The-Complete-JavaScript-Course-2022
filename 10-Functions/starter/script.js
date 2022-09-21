4f3329f3d04b82ff57d29a7c202f2ea3c8b64d24
smcdev <smcdev@outlook.com>
2022-09-20 21:43:23 -0500

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -217,8 +217,7 @@ console.log(addVAT2(23));
 // 3: C++
 // (Write option number)
 // 1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by
-// 1. Make sure to check if the input is a number and if the number makes
-// sense (e.g. answer 52 wouldn't make sense, right?)
+// 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
 // 2. Call this method whenever the user clicks the "Answer poll" button.
 // 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 // 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
@@ -234,6 +233,17 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+};
+
+poll.registerNewAnswer = function () {
+  document
+    .querySelector(".poll")
+    .addEventListener(
+      "click",
+      prompt(
+        "What is your favourite programming language? 0: JavaScript 1: Python 2: Rust 3: C++"
+      )
+    );
 };
 
 ////////////////////////////////////
