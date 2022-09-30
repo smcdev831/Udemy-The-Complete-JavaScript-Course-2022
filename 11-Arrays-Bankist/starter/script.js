36879dc3dd4ab5f4cdd8a83af9f9d9fca182ee30
smcdev <smcdev@outlook.com>
2022-09-30 13:40:38 -0500

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -280,6 +280,7 @@ createUsernames = accounts.forEach((account) => {
     .map((name) => name[0])
     .join("");
 });
+console.log(movements);
 console.log(accounts);
 
 /////////////////////////////////////////////////
@@ -289,6 +290,13 @@ let deposits = movements.filter(function (mov) {
   return mov > 0;
 });
 console.log(deposits);
+
+let depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+let withdrawals = movements.filter((mov) => mov < 0);
+console.log(withdrawals);
 
 /////////////////////////////////////////////////
 // The reduce Method
