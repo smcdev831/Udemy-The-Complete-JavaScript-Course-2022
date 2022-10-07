15a3930f8a4e16468bd8b33ca5b2b3a17b75a1b7
smcdev <smcdev@outlook.com>
2022-10-06 22:58:49 -0500

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -456,6 +456,9 @@ btnLogin.addEventListener("click", function (event) {
     }`;
     containerApp.style.opacity = 100;
 
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur();
+
     displayMovements(currentAccount.movements);
     calcDisplayBalance(currentAccount.movements);
     calcDisplaySummary(currentAccount.movements);
