bd57a22cd362993216578792d3c7e259b7ec57fe
smcdev <smcdev@outlook.com>
2022-10-15 00:48:22 -0500

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -324,8 +324,9 @@ console.log(balance);
 let calcDisplayBalance = function (acc) {
   let balance = movements.reduce((acc, mov) => acc + mov, 0);
   acc.balance = balance;
-  labelBalance.textContent = `${balance} €`;
+  labelBalance.textContent = `${balance}€`;
 };
+
 calcDisplayBalance(account1.movements);
 
 let max = movements.reduce((acc, mov) => (acc > mov ? acc : mov));
@@ -475,6 +476,7 @@ btnTransfer.addEventListener("click", function (e) {
   let receiverAcc = accounts.find(
     (acc) => acc.username === inputTransferTo.value
   );
+
   inputTransferAmount.value = inputTransferTo.value = "";
   if (
     amount > 0 &&
