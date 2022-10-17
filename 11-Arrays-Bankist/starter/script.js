cc67438386d8127ee5d299fff7872a586b6fed3f
smcdev <smcdev@outlook.com>
2022-10-16 22:08:26 -0500

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -401,6 +401,9 @@ let calcDisplaySummary = function (movements) {
   let interest = movements
     .filter((mov) => mov > 0)
     .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
