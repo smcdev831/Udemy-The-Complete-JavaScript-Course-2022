3404e051cd25e8e32367f91bbf79686c75644144
smcdev <smcdev@outlook.com>
2022-10-17 01:03:22 -0500

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -439,6 +439,26 @@ console.log(account);
 /////////////////////////////////////////////////
 // Implementing Login
 
+let currentAccount;
+
+btnLogin.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Login");
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    console.log("Login");
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 100;
+  }
+});
+
 /////////////////////////////////////////////////
 // Implementing Transfers
 
