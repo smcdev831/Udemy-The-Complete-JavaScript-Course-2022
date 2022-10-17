90d29937b58f78d1cabc5fac22d2523a664b1f8d
smcdev <smcdev@outlook.com>
2022-10-16 23:33:50 -0500

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -433,6 +433,9 @@ let firstWithdrawl = movements.find((mov) => mov < 0);
 console.log(movements);
 console.log(firstWithdrawl);
 
+let account = accounts.find((acc) => acc.owner === "Jessica Davis");
+console.log(account);
+
 /////////////////////////////////////////////////
 // Implementing Login
 
