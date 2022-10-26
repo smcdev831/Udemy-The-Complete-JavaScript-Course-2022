6ae887bc69e0fb316f83fba92cb9c99ada2aa369
smcdev <smcdev@outlook.com>
2022-10-26 17:56:58 -0500

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -265,6 +265,21 @@ console.log(0.1 + 0.2 === 0.3);
 console.log(Number("23"));
 console.log(+"23");
 
+console.log(Number.parseInt("30px", 10));
+console.log(Number.parseInt("30px", 2));
+console.log(Number.parseInt("e23", 10));
+console.log(Number.parseInt("e23", 2));
+
+console.log(Number.parseFloat("2.5rem"));
+console.log(Number.parseInt("2.5rem"));
+console.log(Number.parseFloat("     2.5rem     "));
+console.log(parseFloat("     2.5rem     "));
+
+console.log(Number.isNaN(20));
+console.log(Number.isNaN("20"));
+console.log(Number.isNaN(+"20X"));
+console.log(Number.isNaN(23 / 0)); //Infinity
+
 /////////////////////////////////////////////////
 // Math and Rounding
 
