9306081c1ff9b11a14f3b23d1c0be8d8da4ca22a
smcdev <smcdev@outlook.com>
2022-10-27 18:22:02 -0500

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -309,6 +309,20 @@ console.log(Math.min(5, 18, "23px", 11, 2));
 
 console.log(Math.PI * Number.parseFloat("10px") ** 2);
 
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+let randomInt = (min, max) => Math.trunc(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(10, 20));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor("23.9"));
+
 /////////////////////////////////////////////////
 // The Remainder Operator
 
