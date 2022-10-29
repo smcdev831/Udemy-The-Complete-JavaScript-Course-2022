f202a93f18d8adfddc92e30f63e19931528971e5
smcdev <smcdev@outlook.com>
2022-10-28 20:07:13 -0500

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -356,6 +356,7 @@ console.log(isEven(514));
 labelBalance.addEventListener("click", function () {
   [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
     if (i % 2 === 0) row.style.backgroundColor = "orangered";
+    if (i % 3 === 0) row.style.backgroundColor = "blue";
   });
 });
 
