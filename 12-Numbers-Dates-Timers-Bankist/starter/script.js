3edca7248da3ee6f749236072adf6cf71c5436fa
smcdev <smcdev@outlook.com>
2022-10-30 21:23:15 -0500

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -386,6 +386,19 @@ console.log(parseInt("230_000"));
 /////////////////////////////////////////////////
 // Working with BigInt
 
+console.log(2 ** 53 - 1); //bigest number that can be represented
+console.log(Number.MAX_SAFE_INTEGER);
+
+//precision loss
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(4838430248342043823408394839483204);
+console.log(4838430248342043823408394839483204n);
+console.log(BigInt(48384302));
+
 /////////////////////////////////////////////////
 // Creating Dates
 
