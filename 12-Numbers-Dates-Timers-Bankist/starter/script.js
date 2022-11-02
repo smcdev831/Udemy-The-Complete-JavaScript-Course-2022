6a9ec01f44601cfcc0c520b0bec3852d3154a976
smcdev <smcdev@outlook.com>
2022-11-01 21:04:44 -0500

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -81,10 +81,12 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
@@ -142,7 +144,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -461,8 +463,8 @@ updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
 now = new Date();
-let day = now.getDay();
-let month = now.getMonth() + 1;
+let day = `${now.getDay()}`.padStart(2, 0);
+let month = `${now.getMonth() + 1}`.padStart(2, 0);
 let year = now.getFullYear();
 let hour = now.getHours();
 let minute = now.getMinutes();
