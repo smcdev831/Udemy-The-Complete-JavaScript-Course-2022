eb4ab8d0fe1762fd51bd76d64320123e63972d78
smcdev <smcdev@outlook.com>
2022-11-01 19:19:21 -0500

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -421,6 +421,20 @@ console.log(10 / 3);
 /////////////////////////////////////////////////
 // Creating Dates
 
+let now = new Date();
+console.log(now);
+
+console.log(new Date("Nov 01 2022 19:09:25"));
+console.log(new Date("December 24, 2015"));
+
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
 /////////////////////////////////////////////////
 // Adding Dates to 'Bankist' App
 
