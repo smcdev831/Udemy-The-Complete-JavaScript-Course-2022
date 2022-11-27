f5923a705aadd03c9502ac71e81dc7d6fb67d4c3
smcdev <smcdev@outlook.com>
2022-11-26 21:44:56 -0600

    Update script.js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -142,10 +142,18 @@ buttonScrollTo.addEventListener("click", function (e) {
     document.documentElement.clientWidth
   );
 
-  window.scrollTo(
-    s1coords.left + window.pageXOffset,
-    s1coords.top + window.pageYOffset
-  );
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({ behavior: "smooth" });
 });
 
 ///////////////////////////////////////
